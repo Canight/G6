f85cf78a64c3a804446bbd048b4917fb8c444420
wb-xcf804241 <wb-xcf804241@alibaba-inc.com>
2024-04-09 19:58:36 +0800

    feat(behaviors): add lasso-select

--- packages/g6/src/behaviors/index.ts
+++ packages/g6/src/behaviors/index.ts
@@ -6,6 +6,7 @@ export { DragCanvas } from './drag-canvas';
 export { DragElement } from './drag-element';
 export { FocusElement } from './focus-element';
 export { HoverElement } from './hover-element';
+export { LassoSelect } from './lasso-select';
 export { ZoomCanvas } from './zoom-canvas';
 
 export type { BaseBehaviorOptions } from './base-behavior';
@@ -16,4 +17,5 @@ export type { DragCanvasOptions } from './drag-canvas';
 export type { DragElementOptions } from './drag-element';
 export type { FocusElementOptions } from './focus-element';
 export type { HoverElementOptions } from './hover-element';
+export type { LassoSelectOptions } from './lasso-select';
 export type { ZoomCanvasOptions } from './zoom-canvas';
--- packages/g6/src/registry/build-in.ts
+++ packages/g6/src/registry/build-in.ts
@@ -7,6 +7,7 @@ import {
   DragElement,
   FocusElement,
   HoverElement,
+  LassoSelect,
   ZoomCanvas,
 } from '../behaviors';
 import {
@@ -70,6 +71,7 @@ export const BUILT_IN_EXTENSIONS: ExtensionRegistry = {
     'focus-element': FocusElement,
     'create-edge': CreateEdge,
     'brush-select': BrushSelect,
+    'lasso-select': LassoSelect,
   },
   combo: {
     circle: CircleCombo,
